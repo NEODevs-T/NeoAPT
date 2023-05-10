f896b9e035e4ea96ab3e9728dee3d00079124260
JavierTaborda <javieralejandro0999@gmail.com>
2023-05-09 22:00:09 -0400

    Edicion de ResumenPuesto

---
+++ NeoAPTB/DTOs/Grupos.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+namespace NeoAPTB.DTOs
+{
+
+    public partial class Grupos
+    {
+
+        public string? Grupo { get; set; }
+
+        public string? Nomina { get; set; }
+
+
+
+    }
+}
---
+++ NeoAPTB/Pages/AsignacionPuesto.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeoAPTB.Pages
+{
+    public partial class AsignacionPuesto : ComponentBase
+    {
+
+    }
+}
