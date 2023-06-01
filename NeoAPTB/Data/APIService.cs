282259d35f681ebacbfe7277b440858db0717789
Javier Taborda <javieralejandro0999@gmail.com>
2023-06-01 08:19:47 -0400

    Terminar personal

--- NeoAPTB/Data/APIService.cs
+++ NeoAPTB/Data/APIService.cs
@@ -21,8 +21,8 @@ namespace NeoAPTB.Data
         public async Task GetCentros(string cent)
 
         {
-           // var result = await _http.GetFromJsonAsync<List<Centro>>($"http://neo.paveca.com.ve/ReunionApi/Lineas/{cent}");
-            var result = await _http.GetFromJsonAsync<List<Centro>>($"http://localhost:5258/Lineas/{cent}");
+            var result = await _http.GetFromJsonAsync<List<Centro>>($"http://neo.paveca.com.ve/ReunionApi/Lineas/{cent}");
+            //var result = await _http.GetFromJsonAsync<List<Centro>>($"http://localhost:5258/Lineas/{cent}");
             if (result != null)
                 centro = result;
 
@@ -30,24 +30,24 @@ namespace NeoAPTB.Data
 
         public async Task<List<Centro>> GetCentrosxEmpresa(string centro)
         {
-            //var result = await _http.GetFromJsonAsync<List<Centro>>($"http://neo.paveca.com.ve/ReunionApi/Empresas/Centros/{centro}");
-            var result = await _http.GetFromJsonAsync<List<Centro>>($"http://localhost:5258/Empresas/Centros/{centro}");
+            var result = await _http.GetFromJsonAsync<List<Centro>>($"http://neo.paveca.com.ve/ReunionApi/Empresas/Centros/{centro}");
+            //var result = await _http.GetFromJsonAsync<List<Centro>>($"http://localhost:5258/Empresas/Centros/{centro}");
             return result;
         }
 
 
         public async Task GetDivision(string centro, string div)
         {
-           // var result = await _http.GetFromJsonAsync<List<Division>>($"http://neo.paveca.com.ve/ReunionApi/Lineas/Division/{centro}/{div}");
-            var result = await _http.GetFromJsonAsync<List<Division>>($"http://localhost:5258/Lineas/Division/{centro}/{div}");
+            var result = await _http.GetFromJsonAsync<List<Division>>($"http://neo.paveca.com.ve/ReunionApi/Lineas/Division/{centro}/{div}");
+            //var result = await _http.GetFromJsonAsync<List<Division>>($"http://localhost:5258/Lineas/Division/{centro}/{div}");
             if (result != null)
                 divisions = result;
 
         }
         public async Task GetLineas(int div)
         {
-            //var result = await _http.GetFromJsonAsync<List<Linea>>($"http://neo.paveca.com.ve/ReunionApi/Empresas/Lineas/{div}");
-            var result = await _http.GetFromJsonAsync<List<Linea>>($"http://localhost:5258/Empresas/Lineas/{div}");
+            var result = await _http.GetFromJsonAsync<List<Linea>>($"http://neo.paveca.com.ve/ReunionApi/Empresas/Lineas/{div}");
+            //var result = await _http.GetFromJsonAsync<List<Linea>>($"http://localhost:5258/Empresas/Lineas/{div}");
             if (result != null)
                 lineas = result;
 
