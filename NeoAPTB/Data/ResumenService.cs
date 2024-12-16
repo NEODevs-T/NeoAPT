d25bd6a91ccba6edbad2b7e1d5481537f52e735e
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2024-12-16 10:58:42 -0400

    Cambios para visualizar mejora los registros guardados en la pagina consultar puesto, agregacion de una catch si no puede cargar los datos de Tempus, guardado o actulizacion permanete del ususario que modifico los registros

--- NeoAPTB/Data/ResumenService.cs
+++ NeoAPTB/Data/ResumenService.cs
@@ -151,7 +151,7 @@ namespace NeoAPTB.Data
               .Include(r => r.IdMontosNavigation)
               .Include(m => m.IdMontosNavigation.IdPuesTrabNavigation)
               .Include(m => m.IdMontosNavigation.IdLineaNavigation)
-              .Where(r => (r.IdMontosNavigation.IdLineaNavigation.Master.IdLinea == id) & (r.Rfecha >= f1.Date & r.Rfecha <= f2.Date.AddDays(1)))
+              .Where(r => (r.IdMontosNavigation.IdLineaNavigation.Master.IdLinea == id) & (r.Rfecha >= f1.Date & r.Rfecha < f2.Date.AddDays(1)))
               .ToListAsync();
 
             return resumenlineafecha;
